16da65e15437dab7f4ad949e42c716e38703af90
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-16 01:19:22 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -29,7 +29,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   map_public_ip_on_launch = true
   tags = { 
-      Name = "AOE-pub-subnet-${count.index + 1}"
+      Name = "SOK-pub-subnet-${count.index + 1}"
       "kubernetes.io/role/elb" = "1"
       "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
@@ -41,7 +41,7 @@ resource "aws_subnet" "private" {
   availability_zone = data.aws_availability_zones.available.names[count.index]
   vpc_id            = aws_vpc.main.id
   tags = { 
-    Name = "AOE-priv-subnet-${count.index + 1}"
+    Name = "SOK-priv-subnet-${count.index + 1}"
     "karpenter.sh/discovery" = var.cluster_name
     "kubernetes.io/role/internal-elb" = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"

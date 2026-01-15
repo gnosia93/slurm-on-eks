4c01e01759927ba20aaa1dd88f522f0e6c01a59c
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-16 01:05:19 +0900

    Create outputs.tf

---
+++ tf/outputs.tf
@@ -0,0 +1,32 @@
+/*
+output "com_graviton_dns" {
+  value       = aws_instance.graviton_box.public_dns
+  description = "SSH 및 VS Code Server 접속을 위한 EC2 인스턴스의 공인 IP 주소"
+}
+output "com_graviton_vscode" {
+    value = "http://${aws_instance.graviton_box.public_dns}:9090"
+    description = "브라우저에서 VS Code 서버에 접속할 수 있는 URL (PW: 'password' by default)"
+}
+*/
+
+output "com_x86_dns" {
+  value       = aws_instance.x86_box.public_dns
+  description = "SSH 및 VS Code Server 접속을 위한 EC2 인스턴스의 공인 IP 주소"
+}
+output "com_x86_vscode" {
+    value = "http://${aws_instance.x86_box.public_dns}:9090"
+    description = "브라우저에서 VS Code 서버에 접속할 수 있는 URL (PW: 'password' by default)"
+}
+
+
+output "public_subnet" {
+    description = "퍼블릭 서브넷 ID 목록"
+    value       = aws_subnet.public[*].id
+}
+
+output "private_subnet" {
+    description = "프라이빗 서브넷 ID 목록"
+    value       = [for subnet in aws_subnet.private : subnet.id]
+}
+
+
